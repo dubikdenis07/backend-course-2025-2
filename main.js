a48978c11b44cf7e8ae28ec6cd156537fdb4b1d3
Денис bc2025-2 <dubikdenis07@gmail.com>
2025-09-16 22:26:53 +0200

    оновив код

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-// Константи для варіанту 6
-const date = "20231001";       // 1 жовтня 2023 (формат YYYYMMDD)
-const time_period = "q";       // квартал
+const date = "20231001";
+const time_period = "q";
+const url = new URL(
+  "https://bank.gov.ua/NBUStatService/v1/statdirectory/basindbank",
+);
 
-// Створюємо об'єкт URL
-const url = new URL("https://bank.gov.ua/NBU_Statistics/Opendata/api/Financial_Soundness");
-
-// Додаємо параметри через searchParams
 url.searchParams.append("date", date);
 url.searchParams.append("period", time_period);
 url.searchParams.append("json", "");
 
-// Виводимо готовий URL
-console.log("Сформований URL:");
-console.log(url.toString());
+console.log(url.href);
